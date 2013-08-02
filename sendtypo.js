16b51dfeb45f24d5c7a3601bf32293d7a42db273
Kellen Fujimoto <kellen@kellenfujimoto.com>
2013-08-02 16:14:46 -0700

    Added license, fixed bug
    Now using the BSD 2-Clause License!
    (http://opensource.org/licenses/BSD-2-Clause)
    
    Fixed the bug which would cause the bubble in the upper left to not
    disappear properly. Adding parentheses to the window.getSelection()
    call fixed this problem.

--- sendtypo.js
+++ sendtypo.js
@@ -1,19 +1,32 @@
+/*
+"Tyop" Alpha by Kellen Fujimoto (http://kellenfujimoto.com/)
+
+Copyright (c) 2013, Kellen Fujimoto
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 $(document).ready(function() {
 // Add HTML to document (yes, it's ugly)
-$("h1").before('<div id="typo" style="position:fixed;top:5px;left:5px"><button name="send" type="submit" value="Send Typo" id="sendTypo" style="width:100px;height:33px;font:inherit;background:white;color:rgb(10,10,10);border: 1px solid rgb(10,10,10);border-radius:3px;-webkit-border-radius:3px;">Send Typo</button></div>');
+$("h1").before('<div id="typo" style="position:fixed;top:5px;left:5px"><button name="send" type="submit" value="Send Typo" id="sendTypo" style="padding:1.5em;font:inherit;background:white;color:rgb(10,10,10);border: 1px solid rgb(10,10,10);border-radius:3px;-webkit-border-radius:3px;">Send Feedback</button></div>');
 
 // Hide button
 $("#typo").fadeOut(250);
 // Catching selection of text to display send button
 $("p").mouseup(function() {
-    if (window.getSelection) {
-        $("#typo").fadeIn(1500);
+    if (window.getSelection() != "") {
+        $("#typo").fadeIn(750);
     }
 });
 
-$("p").mouseup(function() {
-    if (!window.getSelection) {
-        $("#typo").fadeOut(3000);
+$("html").mouseup(function() {
+    if (window.getSelection() == "") {
+        $("#typo").fadeOut(1750);
     }
 });
 // The function to send the typo in an email
